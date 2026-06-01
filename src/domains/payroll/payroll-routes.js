914028a69516481449c3edd1335202923c86e6fe
Hikmat Arif Nugraha <hikmatngrha@gmail.com>
2026-06-01 16:08:43 +0700

    add logic download pdf

--- src/domains/payroll/payroll-routes.js
+++ src/domains/payroll/payroll-routes.js
@@ -13,6 +13,11 @@ class PayrollRoutes extends BaseRoutes {
 			tryCatch(PayrollController.list),
 		]);
 
+		this.router.get("/:id/download-pdf", [
+			authTokenMiddleware.authenticate,
+			tryCatch(PayrollController.downloadPdf),
+		]);
+
 		this.router.get("/:id", [
 			authTokenMiddleware.authenticate,
 			tryCatch(PayrollController.detail),
